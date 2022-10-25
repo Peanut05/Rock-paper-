4fb15806b15f57a8a079b2a70cb49ae3c8ecb270
krushna Gavali <krushnagawali744@gmail.com>
2022-10-25 14:21:46 +0530

    Update Game.py
    list of players

--- Game.py
+++ Game.py
@@ -6,3 +6,9 @@ root = Tk()
 # Set geometry
 root.geometry("500x500")
 root.title("Rock-Paper-Scissors-Game")
+# List of players
+list = ["rock","paper","scissors"]
+choose_number = randint(0,2)
+print(choose_number) # For testing if it works
+label = Label(root,text="select your choice",width = 20,height=4,font=("calibri",15))
+label.pack()
